d5e148b11ee8ad6e0ba411f5de08ab970ba652e3
Lily Takahari <44313208+lilytakahari@users.noreply.github.com>
2020-02-26 22:04:15 -0800

    add commenting

--- Actor.cpp
+++ Actor.cpp
@@ -1,10 +1,11 @@
 #include "Actor.h"
 #include "StudentWorld.h"
 
-// Students:  Add code to this file, Actor.h, StudentWorld.h, and StudentWorld.cpp
 #include "GameConstants.h"
 #include <iostream>
 
+// canMoveTo: if the moving actor can move to the destination, move there
+//            else return false
 bool Movers::canMoveTo(double destX, double destY)
 {
     if (getWorld()->canMove(this, destX, destY)) {
@@ -14,6 +15,7 @@ bool Movers::canMoveTo(double destX, double destY)
     return false;
 }
 
+// Penelope constructor: initiate with goodie charges set to 0
 Penelope::Penelope(StudentWorld *world, double startX, double startY)
 : Human(world, IID_PLAYER, startX, startY,
         right, 0)
@@ -22,6 +24,9 @@ Penelope::Penelope(StudentWorld *world, double startX, double startY)
     m_flames = 0;
     m_vaccines = 0;
 }
+
+// awardGoodie: other actors may call this function
+//              to tell Penelope to increase goodie charges
 void Penelope::awardGoodie(char type)
 {
     switch (type) {
@@ -37,11 +42,17 @@ void Penelope::awardGoodie(char type)
     }
 }
 
+// Steps:
+// 1. Check if alive
+// 2. Infected
+// 3. Player input - may generate new actors
 void Penelope::doSomething()
 {
     StudentWorld* myWorld = getWorld();
+    // 1
     if (!stillAlive())
         return;
+    // 2
     if (isInfected())
     {
         zombify();
@@ -50,8 +61,9 @@ void Penelope::doSomething()
             return;
         }
     }
-    int ch;
     
+    // 3
+    int ch;
     if (myWorld->getKey(ch))
     {
         double currX = getX();
@@ -132,22 +144,32 @@ void Penelope::doSomething()
     }
 }
 
+// Steps:
+// 1. If overlap with citizen
+// 2. If overlap with Penelope and all citizens escaped
 void Exit::doSomething()
 {
+    // 1
     StudentWorld* myWorld = getWorld();
     if (myWorld->citizenEscapes(this))
     {
         myWorld->increaseScore(500);
         myWorld->playSound(SOUND_CITIZEN_SAVED);
     }
+    // 2
     if (myWorld->allEscaped() && myWorld->overlapPenelope(this))
         myWorld->nowPassed();
 }
 
+// Steps:
+// 1. Check if alive
+// 2. If overlap Penelope
 void Goodie::doSomething()
 {
+    // 1
     if (!stillAlive())
         return;
+    // 2
     StudentWorld* myWorld = getWorld();
     if (myWorld->overlapPenelope(this))
     {
@@ -158,6 +180,8 @@ void Goodie::doSomething()
     }
 }
 
+// tellWorld: variously implemented based on what the 
+// specific type of goodie awards to Penelope
 void GasCanGoodie::tellWorld() {
     getWorld()->awardGoodie('f');
 }
@@ -168,27 +192,39 @@ void VaccineGoodie::tellWorld() {
     getWorld()->awardGoodie('v');
 }
 
+// Steps:
+// 1. Check if alive
+// 2. Damage
+// 3. Decrement health
 void Dmgers::doSomething() {
+    // 1 
     if (!stillAlive())
         return;
+    // 2
     damage();
+    // 3
     decHealth();
 }
 
+// damage: variously implemented based on what the 
+// specific type of damager does to actors
 void Vomit::damage() {
     getWorld()->infectActors(this);
 }
-
 void Flame::damage() {
     getWorld()->killActors(this);
 }
 
+// getKilled: other actors may call this function
+//            to tell Penelope to die
 bool Penelope::getKilled() {
     getWorld()->playSound(SOUND_PLAYER_DIE);
     setDead();
     return true;
 }
 
+// getKilled: shared dying actions among dumb
+//            and smart zombies
 bool Zombie::getKilled()
 {
     getWorld()->playSound(SOUND_ZOMBIE_DIE);
@@ -196,15 +232,26 @@ bool Zombie::getKilled()
     return true;
 }
 
+// Steps:
+// 1. Check if alive
+// 2. If skip move or not
+// 3. Try to vomit
+// 4. Determine new movement plan if needed
+// 5. Determine destination location
+// 6. If not blocked, move there
+// 7. Else determine new movement in next tick
 void Zombie::doSomething()
 {
+    // 1
     if (!stillAlive())
         return;
+    // 2
     if (m_skipMove) {
         m_skipMove = false;
         return;
     }
     m_skipMove = true;
+    // 3
     double vomitX, vomitY;
     computeThrowLoc(getDirection(), vomitX, vomitY);
     StudentWorld* myWorld = getWorld();
@@ -217,16 +264,19 @@ void Zombie::doSomething()
             return;
         }
     }
+    // 4
     if (m_moveplan == 0)
     {
         m_moveplan = randInt(3, 10);
         setDirection(determineDirection());
     }
+    // 5
     double currX = getX();
     double currY = getY();
     bool couldMove = false;
     switch (getDirection())
     {
+        // 6
         case up:
             couldMove = canMoveTo(currX, currY+1);
             break;
@@ -240,17 +290,21 @@ void Zombie::doSomething()
             couldMove = canMoveTo(currX+1, currY);
             break;
     }
+    // 7
     if (!couldMove)
         m_moveplan = 0;
     else
         m_moveplan--;
 }
 
+// randDir: returns a random direction
 int Zombie::randDir() {
     int dirs[4] = {up, down, left, right};
     return dirs[randInt(0, 3)];
 }
 
+// computeThrowLoc: computes the location to spawn something at
+//                  based on the passed in dir
 void Zombie::computeThrowLoc(int dir, double& x, double& y)
 {
     double throwX = getX();
@@ -274,6 +328,13 @@ void Zombie::computeThrowLoc(int dir, double& x, double& y)
     y = throwY;
 }
 
+// getKilled: other actors may call this function
+//            to tell the zombie to die
+//            dumb zombies have the chance to spawn
+//            a vaccine upon being killed
+// NOTE: the spec says to spawn the vaccine at the
+//       zombie's current location, but I think the intended
+//       and correct implementation was to throw it out in a direction
 bool DumbZombie::getKilled()
 {
     Zombie::getKilled();
@@ -289,28 +350,39 @@ bool DumbZombie::getKilled()
     return true;
 }
 
+// damage: a pit kills any actors that overlap it
 void Pit::damage() {
     getWorld()->killActors(this);
 }
 
+// Steps:
+// 1. Check if alive
+// 2. Decrement safety ticks if not active yet
+// 3. If overlaps movers or Penelope, becomes active by exploding
 void Landmine::doSomething()
 {
+    // 1
     if (!stillAlive())
         return;
+    // 2
     if (m_safety > 0) {
         m_safety--;
         return;
     }
+    // 3
     if (getWorld()->overlapPenelope(this)
         || getWorld()->overlapMover(this))
         explode();
 }
+
+// getKilled: if hit by a flame, it will explode
 bool Landmine::getKilled()
 {
     explode();
     return true;
 }
 
+// explode: landmine dies and generates flames around it
 void Landmine::explode()
 {
     setDead();
@@ -330,6 +402,8 @@ void Landmine::explode()
     myWorld->createActorAt('p', currX, currY, up);
 }
 
+// getKilled: other actors may call this function
+//            to tell the zombie to die
 bool SmartZombie::getKilled()
 {
     Zombie::getKilled();
@@ -337,6 +411,8 @@ bool SmartZombie::getKilled()
     return true;
 }
 
+// determineDirection: goes toward the nearest human if they are close
+//                     else choose a random direction
 int SmartZombie::determineDirection()
 {
     double humanX, humanY, humanDist;
@@ -353,6 +429,7 @@ int SmartZombie::determineDirection()
     }
 }
 
+// getKilled: citizen died, oh no
 bool Citizen::getKilled()
 {
     setDead();
@@ -362,11 +439,21 @@ bool Citizen::getKilled()
     return true;
 }
 
+// Steps:
+// 1. Check if alive
+// 2. Maybe zombify
+// 3. If skip move or not
+// 4. Determine distance to Penelope
+// 5. Determine distance to nearest zombie
+// 6. Maybe move toward Penelope
+// 7. Maybe run away from zombies
 void Citizen::doSomething()
-{
+{   
+    // 1
     if (!stillAlive())
         return;
     StudentWorld* myWorld = getWorld();
+    // 2
     if (isInfected())
     {
         zombify();
@@ -382,19 +469,25 @@ void Citizen::doSomething()
             return;
         }
     }
+    // 3
     if (m_skipMove)
     {
         m_skipMove = false;
         return;
     }
     m_skipMove = true;
+    
     double pX, pY, pDist;
     double zX, zY, zDist;
     double currX = getX();
     double currY = getY();
+    // 4
     if (!myWorld->distanceToPenelope(this, pX, pY, pDist))
         return;
+    // 5
     bool zExists = myWorld->findNearestZombie(currX, currY, zX, zY, zDist);
+    
+    // 6
     if ((!zExists || (pDist < zDist)) && pDist <= 80)
     {
         int otherDir;
@@ -414,10 +507,10 @@ void Citizen::doSomething()
         }
     } else if (!zExists)
         return;
+    
+    // 7
     if (zDist <= 80)
     {
-        
-       
         double fubar = VIEW_HEIGHT*VIEW_WIDTH;
         double distAllDirZombies[4] = {fubar, fubar, fubar, fubar};
         double potX[4] = {currX, currX, currX-2, currX+2};
@@ -442,10 +535,10 @@ void Citizen::doSomething()
         int otherDir;
         int moveDir = reverseDirection(determineFollowDirection(zX, zY, otherDir, stay));
         moveInDir(moveDir);
-        
     }
 }
 
+// moveInDir: move in the given dir
 bool Citizen::moveInDir(int dir)
 {
     double currX = getX();
@@ -471,6 +564,8 @@ bool Citizen::moveInDir(int dir)
     return couldMove;
 }
 
+// determineFollowDirection: determines the direction that the current mover
+//                           should move in, in order to get closer to (x, y)
 int Movers::determineFollowDirection(double x, double y, int &otherDir, bool &twoDir)
 {
     int dir[2];
@@ -511,6 +606,9 @@ int Movers::determineFollowDirection(double x, double y, int &otherDir, bool &tw
     }
 }
 
+// reverseDirection: so that a Citizen can use
+// determineFollowDirection, then reverse that given direction
+// to yeet themself away from the zombie
 int Citizen::reverseDirection(int dir)
 {
     switch (dir) {
